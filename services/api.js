06772b13f159cb1e852802ca7f9354524f35348e
Fernando Laterza <fhlaterza@gmail.com>
2024-11-10 19:22:21 -0300

    Projeto finalizado

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://eftcaqtfmaxoygwxcyhs.supabase.co/rest/v1/',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmdGNhcXRmbWF4b3lnd3hjeWhzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMTI2MzQ0OSwiZXhwIjoyMDQ2ODM5NDQ5fQ.HVYc29zIk5i4_7xQQhoH7T2TKP-EZtk85DR6Lc6fS2M",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmdGNhcXRmbWF4b3lnd3hjeWhzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMTI2MzQ0OSwiZXhwIjoyMDQ2ODM5NDQ5fQ.HVYc29zIk5i4_7xQQhoH7T2TKP-EZtk85DR6Lc6fS2M"
     }
 })
